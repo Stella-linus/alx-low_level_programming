aa1768b83c6e27ceeca5218356ac4e9e18321f4c
Stella-linus <stellalinus1225@gmail.com>
2023-04-23 16:20:35 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
