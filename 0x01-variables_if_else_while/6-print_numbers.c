100aaa97aea7ca2382d0044f776c6bd121c91161
Stella-linus <stellalinus1225@gmail.com>
2023-04-24 19:42:21 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print number from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{       
+        int i;
+        
+        for (i = 48; i < 158; i++)
+        {
+                printf(i);
+        }       
+        putchar('\n');
+        return (0)
+}               
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible comvinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{       
+        int i;
+        
+        for (i = 48; i < 58; i++)
+        {
+                putchar(i);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+        }
+        putchar('\n');
+        return (0)
+}     
