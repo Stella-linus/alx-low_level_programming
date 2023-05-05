e5b50e519f3e47773ad729d27978fe702153c93b
Stella-linus <stellalinus1225@gmail.com>
2023-05-05 08:19:45 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncpy - copy a strings
  * @dest: input value
@@ -20,9 +21,8 @@ char *_strncpy(char *dest, char *src, int n)
 	while (j < n)
 	{
 		dest[j] = '\0';
-		j++
+		j++;
 	}
 
 	return (dest);
 }
-
